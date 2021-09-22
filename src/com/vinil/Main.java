83b0887cfb4bd63bc3272c5ea8e51c7207f83954
viniljose <vinilpj@gmail.com>
2021-09-22 19:36:18 +0530

    Create Main.java

---
+++ src/com/vinil/Main.java
@@ -0,0 +1,12 @@
+package com.vinil;
+
+/**
+ * https://openjdk.java.net/projects/jdk/17/
+ * https://openjdk.java.net/projects/jdk/17/jeps-since-jdk-11
+ */
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
