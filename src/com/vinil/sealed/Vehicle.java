9a26ba1b60b5e123160dc2b0b7b0a63db65f3172
viniljose <vinilpj@gmail.com>
2021-09-22 22:33:01 +0530

    Create Vehicle.java

---
+++ src/com/vinil/sealed/Vehicle.java
@@ -0,0 +1,13 @@
+package com.vinil.sealed;
+
+public abstract sealed class Vehicle permits Car, Truck{
+    protected final String registrationNumber;
+
+    public Vehicle(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+}
