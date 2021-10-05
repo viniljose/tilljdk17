a90966f414562c4fb92ebd0bb7f6e263556b2605
viniljose <vinilpj@gmail.com>
2021-10-05 12:09:27 +0530

    Create TestRecord.java

---
+++ src/com/vinil/record/TestRecord.java
@@ -0,0 +1,8 @@
+package com.vinil.record;
+
+public class TestRecord {
+    public static void main(String[] args) {
+        Person person = new Person("Vinil Jose","Bangalore");
+        System.out.println(person);
+    }
+}
